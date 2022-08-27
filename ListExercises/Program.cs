72fadcbac091a7608ba1e13065f940edaae3cc3d
Chamane Barbatti <chamane@sbcglobal.net>
2022-08-27 12:53:27 -0500

    finished

--- ListExercises/Program.cs
+++ ListExercises/Program.cs
@@ -7,10 +7,10 @@ namespace ListExercises
     {
         static void Main(string[] args)
         {
-           
-            List<int> numbers = new List<int> {1,2,3,4,5,6,7,8,9,10 };
-          int total = 0;
-                foreach (int number in numbers)
+
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int total = 0;
+            foreach (int number in numbers)
             {
                 if (number % 2 == 0)
                 {
@@ -22,15 +22,26 @@ namespace ListExercises
 
 
 
-        }
 
-        static void printFiveLetterWords(List<string> wordlist)
-        {
-            foreach (string word in wordlist)
+            List<string> word = new List<string>();
+            word.Add("Hello");
+            word.Add("Four");
+            word.Add("Hi");
+            foreach (string w in word) {
+                Console.WriteLine(w);
+    }
+            Console.WriteLine("using this method");
+            printFiveLetterWords(word);
+            
+            static void printFiveLetterWords(List<string> wordlist)
             {
-                if (word.Length == 5)
+
+                foreach (string word in wordlist)
                 {
-                    Console.WriteLine(word);
+                    if (word.Length == 5)
+                    {
+                        Console.WriteLine(word);
+                    }
                 }
             }
         }
