1f1f5a31cea41834dd99966fc947a737254ab38f
Chamane Barbatti <chamane@sbcglobal.net>
2022-08-27 01:05:08 -0500

    finished Excercises

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -6,7 +6,13 @@ namespace ArrayExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Arrays!");
+            int[] array  = new int[6];
+            int[] array1  = { 1, 1, 2, 3, 5, 8 };
+            foreach (int num in array1) 
+            {
+                Console.WriteLine(num);
+
+            }
         }
     }
 }
--- ListExercises/Program.cs
+++ ListExercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ListExercises
 {
@@ -6,7 +7,33 @@ namespace ListExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Chamane!");
+           
+            List<int> numbers = new List<int> {1,2,3,4,5,6,7,8,9,10 };
+          int total = 0;
+                foreach (int number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    total += number;
+                }
+            }
+            Console.WriteLine(total);
+
+
+
+
         }
+
+        static void printFiveLetterWords(List<string> wordlist)
+        {
+            foreach (string word in wordlist)
+            {
+                if (word.Length == 5)
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        }
+
     }
 }
